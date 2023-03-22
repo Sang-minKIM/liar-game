1b30fc3d5dfb879890fa406471c08503205fb558
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-03-22 19:49:15 +0900

    add: css reset, theme

---
+++ src/styled.d.ts
@@ -0,0 +1,11 @@
+import "styled-components";
+
+declare module "styled-components" {
+    export interface DefaultTheme {
+        black: string;
+        blue: string;
+        lightBlue: string;
+        gray: string;
+        darkGray: string;
+    }
+}
---
+++ src/theme.tsx
@@ -0,0 +1,7 @@
+export const theme = {
+    black: "#1D1D20",
+    blue: "#32ADE6",
+    lightBlue: "#80CEF0",
+    gray: "#E5E5EA",
+    darkGray: "#AEAEB2",
+};
