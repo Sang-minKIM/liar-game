6d2ae7a7115701fb77b41446be6b35b65c33e94e
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2023-03-26 17:17:53 +0900

    fix : game page input required

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, ContentsBox, DogImg, Message, Submit } from "../components/Contents";
+import { Container, ContentsBox, DogImg, Message } from "../components/Contents";
 import dog from "../assets/Dog.png";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
@@ -11,7 +11,7 @@ const GameContents = styled(ContentsBox)`
 
 const Answer = styled.input`
     width: 88%;
-    height: 15%;
+    height: 40%;
     margin: 1rem 0;
     border: 1px solid ${(props) => props.theme.lightBlue};
     border-radius: 16px;
@@ -25,8 +25,33 @@ const Answer = styled.input`
     }
 `;
 
+const AnswerForm = styled.form`
+    width: 100%;
+    height: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Submit = styled.input`
+    width: 12rem;
+    height: 3rem;
+    border-radius: 0.75rem;
+    border: none;
+    background-color: ${(props) => props.theme.blue};
+    font-size: ${(props) => props.theme.fontSize.large};
+    color: ${(props) => props.theme.white};
+    box-shadow: 0px 0px 3px 0px ${(props) => props.theme.darkGray};
+    &:active {
+        opacity: 0.8;
+        box-shadow: none;
+    }
+`;
+
 function Game() {
     const navigate = useNavigate();
+
     return (
         <Container>
             <GameContents>
@@ -34,8 +59,10 @@ function Game() {
                 <Message>라이어로</Message>
                 <Message>지목된 사람은</Message>
                 <Message>제시어를 적어줘</Message>
-                <Answer placeholder="제시어는..."></Answer>
-                <Submit onClick={() => navigate("/finish")}>확인</Submit>
+                <AnswerForm onSubmit={() => navigate("/finish")}>
+                    <Answer required placeholder="제시어는..."></Answer>
+                    <Submit type="submit" name="answer" value="확인" />
+                </AnswerForm>
             </GameContents>
         </Container>
     );
